0206b5aef2affad004d8aeeaf43d7af9f09fbd5b
Iteoluwakiishi <iteoluwaadejobi@gmail.com>
2026-06-21 23:39:54 +0100

    logo

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import logo from './assets/LogoR2.png'
+import logo from './assets/logoR2.png'
 
 function NavHead({ scrolled }) {
   const [menuOpen, setMenuOpen] = useState(false)
